6f0cc3ba256af4dd874df974af790ff68fce4a24
rusctzec <rusctzec@protonmail.com>
2020-05-27 00:47:54 -0700

    Add built application for deployment to Git Pages

---
+++ precache-manifest.0efe5e9210ef8e40fe99621b0076c218.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb60c0ed95e30b71424de47c4975203",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c4adb4e28cc4c70be4f",
+    "url": "/static/css/main.cf453399.chunk.css"
+  },
+  {
+    "revision": "5f4b41b494e7072e75a7",
+    "url": "/static/js/2.3ba27e0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3ba27e0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c4adb4e28cc4c70be4f",
+    "url": "/static/js/main.330c62d4.chunk.js"
+  },
+  {
+    "revision": "7c815cba8b9356696961",
+    "url": "/static/js/runtime-main.41240b73.js"
+  }
+]);
